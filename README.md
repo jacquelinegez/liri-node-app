e23c0813907081f11a56e24184f0e6d184ad2ade
Jacque <jacquelinegez@icloud.com>
2019-05-07 23:22:01 -0400

    added link

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # liri-node-app
+
+Link to demo : https://drive.google.com/file/d/1iYq1ZSANxENYeCE6p-niOj_5qmA-V5ID/view
--- liri.js
+++ liri.js
@@ -1,24 +1,125 @@
 require("dotenv").config();
-var fs = require("fs");
-var spotify = require("spotify");
-var action = process.argv[2];
-var request = require("request");
 
-switch (command) {
+var keys = require("./keys.js");
+var fs = require("fs");
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
+var axios = require("axios");
+var command = process.argv[2];
+var input = process.argv[3];
+var moment = require("moment");
+
+//Process the command from node input line
+  switch (command) {
     case "concert-this":
-      artistConcerts();
+      bandsInTownArtistEvents();
       break;
     case "spotify-this-song":
-      searchSong();
+      spotifySong();
       break;
     case "movie-this":
       moviesOMDB();
       break;
     case "do-what-it-says":
-      doWhatItSays();
+     doWhatItSay();
       break;
   }
 
-  function artistConcerts () {
-      
-  }
+function bandsInTownArtistEvents() {
+
+  // Then run a request with axios to the Bands in Town Artist Events API with the movie specified
+  var queryUrl = "https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp"
+
+   // This line is just to help us debug against the actual URL.
+   //console.log(queryUrl);
+  axios.get(queryUrl).then(
+
+    function(response) {
+
+      //Printing out the list of the upcomint artist/band conserts.
+      for(var i = 0; i < response.data.length; i++) {
+        console.log("---------------------");
+        console.log("Name of the venue: " + response.data[i].venue.name);
+        console.log("Venue location for event: " + response.data[i].venue.city);
+        console.log("Date of the event: " + moment(response.data[i].datetime).format("MM/DD/YYYY"));
+        console.log("---------------------");
+      }
+    }
+  );
+}
+
+
+function moviesOMDB() {
+
+  //If user doesn't specify the movie name it will automatic type info for titanic movie
+  if (!input){
+    console.log("If you haven't watched 'Mr. Nobody,' then you should. It's on Netflix!");
+    input = "Mr. Nobody";
+  };
+
+   // Then run a request with axios to the OMDB API with the movie specified
+  var queryUrl = "http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=trilogy";
+
+   // This line is just to help us debug against the actual URL.
+   //console.log(queryUrl);
+
+  axios.get(queryUrl).then(
+    function(response) {
+
+      //Printing out the movie info
+      console.log("----------------------");
+      console.log("Title of the movie: " + response.data.Title);
+      console.log("Year the movie came out: " + response.data.Released);
+      for(var i = 0; i < (response.data.Ratings.length - 2); i++) {
+        console.log("IMDB Rating of the movie: " + response.data.Ratings[0].Value);
+        console.log("Rotten Tomatoes Rating of the movie: " + response.data.Ratings[1].Value);
+      }
+      console.log("Country where the movie was produced: " + response.data.Country);
+      console.log("Language of the movie: " + response.data.Language);
+      console.log("Plot of the movie: " + response.data.Plot);
+      console.log("Actors in the movie: " + response.data.Actors);
+      console.log("----------------------");
+    }
+  );
+}
+
+function doWhatItSay() {
+
+  // This block of code will read from the "random.txt" file.
+  // The code will store the contents of the reading inside the variable "data"
+
+  fs.readFile("random.txt", "utf8", function(error, data) {
+
+    if(error) {
+      return console.log(error);
+    }
+
+    var dataArr = data.split(",");
+    input = dataArr[1];
+    console.log(input);
+    spotifySong(input);
+  });
+}
+
+function spotifySong() {
+    ////If user has not specified a song , default to "I will always love you" by Witney Houston
+    if (!input){
+      console.log("Search this song!");
+      input = "I Want it That Way";
+    }
+
+  spotify.search({ type: 'track', query: input }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+
+    var songInfo = data.tracks.items;
+    console.log("------------------------");
+    console.log("Artist name: " + songInfo[0].artists[0].name); 
+    console.log("Song Name: " + songInfo[0].name);
+    console.log("Preview Link: " + songInfo[0].preview_url);
+    console.log("Album: " + songInfo[0].album.name);
+    console.log("------------------------");
+  });
+}
+
